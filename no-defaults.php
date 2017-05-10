b0e6e2351355ef59d0addf75c7f758442147cfa2
Shivanand Sharma <shivanand@binaryturf.com>
2017-05-10 17:40:02 +0000

    Add plugin base file

---
+++ no-defaults.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package WP_No_Defaults
+ * @version 1.6
+ */
+/*
+Plugin Name: WP No Defaults
+Plugin URI: https://www.binaryturf.com/no-defaults/
+Description: Kill the default sample page and post on fresh installs
+Author: Shivanand Sharma
+Version: 1.0
+Author URI: https://www.binaryturf.com
+*/
+
+function wp_install_defaults(){}
